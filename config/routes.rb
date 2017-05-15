00f2d07c1147c1f547b3c5227641e6cd8876b84b
Aurore Courtois <aurore.courtois@outlook.fr>
2017-05-15 16:41:06 +0200

    Resolve Longest Word Game

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'words/game' => 'words#game'
+
+  get 'words/score' => 'words#score'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
